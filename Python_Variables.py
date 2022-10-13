8c88989a4e968d6e49e39e4ceab78198e3742345
Dobinda-Alessandro <dobinda.alessandro@iisgalvanimi.edu.it>
2022-10-13 10:50:24 +0000

    esercitazione_1

---
+++ Python_Variables.py
@@ -0,0 +1,31 @@
+#creo due variabili e stampo le due variabili 
+x= 45 
+y= "carlo"
+print(x)
+print(y)
+
+
+a = str(3)   # dico che il numero è una stringa "3"
+b = int(3)   # dico che un numero è intero
+c = float(3) # dico che il numero è con la virgola 
+print(a)
+print(b)
+print(c)
+
+#se uso type() mi dice che tipo di variabile è
+print(type(x))
+print(type(y))
+print(type(a))
+print(type(b))
+print(type(c))
+
+# se metto due virgolette o una virgoletta la variabile rimane una stringa 
+y = "carlo"
+y = 'carlo'
+
+#se creo la variabile d minuscola e D maiuscola sono differenti
+d = 7
+D = "marco"
+print("D")
+print(d)
+
