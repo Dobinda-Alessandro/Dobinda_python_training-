b522f9d09f59f393e44a3d66c78b471442110913
Dobinda-Alessandro <dobinda.alessandro@iisgalvanimi.edu.it>
2022-10-20 10:51:53 +0000

    versione 1

---
+++ main_2.py
@@ -0,0 +1,19 @@
+# Creo un elenco che mi permette di associare più elementi ad una variabile 
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+#lenght (len) mi dice quanti elementi ci sono dentro alla mia lista 
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+#list liste 
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+print(list1)
+print(list2)
+print(list3)
+
+# type ci dice che tipo è la nostra variabile 
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
